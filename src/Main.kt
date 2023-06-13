83777bf770de52201b92229379ff3a74064be988
Gabriel <podryto@protonmail.com>
2023-06-13 16:50:01 -0300

    Example implemented.

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,16 @@
 import br.org.example.dominio.*
 
 fun main() {
-    
+    val alunosTeste = setOf(Aluno("NomeAluno0", "emailaluno0"),
+                             Aluno("NomeAluno1", "emailaluno1"),
+                             Aluno("NomeAluno2", "emailaluno2"),
+                             Aluno("NomeAluno3", "emailaluno3"),
+                             Aluno("NomeAluno4", "emailaluno4"))
+    val conts = setOf(ConteudoEducacional("Sintaxe Java", Nivel.BASICO, 120),
+                        ConteudoEducacional("Sintaxe Kotlin", Nivel.BASICO, 150),
+                        ConteudoEducacional("Spring, do básico ao avançado", Nivel.BASICO, 250),
+                        ConteudoEducacional("Git avançado", Nivel.INTERMEDIARIO, 50))
+    val formacao0 = Formacao("Backend Java/Kotlin", conts)
+    formacao0.matricular(alunosTeste)
+    println(formacao0)
 }
--- src/br/org/example/dominio/ConteudoEducacional.kt
+++ src/br/org/example/dominio/ConteudoEducacional.kt
@@ -3,7 +3,7 @@ package br.org.example.dominio
 enum class Nivel { SEM_NIVEL, BASICO, INTERMEDIARIO, DIFICIL }
 
 data class ConteudoEducacional(val titulo: String = "Sem título",
-                          val nivel: Nivel = Nivel.BASICO,
+                          val nivel: Nivel = Nivel.SEM_NIVEL,
                           val cargaHoraria: Int = 0)
 {
 }
